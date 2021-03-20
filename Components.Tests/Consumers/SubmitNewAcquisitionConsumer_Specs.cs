2e233003e0791fe5ac467909b76fc347f088f12e
Tyler Schock <schock3736@gmail.com>
2021-03-20 17:32:05 -0500

    Setup Tests project structure

---
+++ Components.Tests/Consumers/SubmitNewAcquisitionConsumer_Specs.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Components.Tests.Consumers
+{
+    public class SubmitNewAcquisitionConsumer_Specs
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
--- Components.Tests/StateMachines/OrderStateMachine_Specs.cs
+++ Components.Tests/StateMachines/OrderStateMachine_Specs.cs
@@ -1,6 +1,6 @@
 using NUnit.Framework;
 
-namespace Components.Tests
+namespace Components.Tests.StateMachines
 {
     public class OrderStateMachineTests
     {
